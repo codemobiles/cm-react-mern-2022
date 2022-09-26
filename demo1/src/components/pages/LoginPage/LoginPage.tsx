6418e76ecf5f6dc66fe4b103a21c0a8f5e5ce183
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-09-26 15:14:46 +0700

    1234

--- demo1/src/components/pages/LoginPage/LoginPage.tsx
+++ demo1/src/components/pages/LoginPage/LoginPage.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@mui/material";
 import * as React from "react";
 
 type LoginPageProps = {
@@ -5,7 +6,12 @@ type LoginPageProps = {
 };
 
 const LoginPage: React.FC<any> = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div>
+      LoginPage1
+      <Button onClick={() => alert("1234")}>Ok </Button>
+    </div>
+  );
 };
 
 export default LoginPage;
