e5b272a010e334d018f35e1ad0304f62a1caee2c
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-09-26 10:16:46 +0700

    1234

--- demo1/src/App.tsx
+++ demo1/src/App.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 export default function App({}: Props) {
-  return (
-    <div>App</div>
-  )
+  let result = "";
+  result = getResult();
+  return <div>App</div>;
+}
+
+function getResult() {
+  return 1;
 }
