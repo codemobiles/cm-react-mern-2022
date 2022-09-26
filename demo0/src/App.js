f48a269677ad1dc6a7d30c8b23e4b3ea63bd6988
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-09-26 10:03:54 +0700

    1234

--- demo0/src/App.js
+++ demo0/src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 
 export default function App() {
+  let count = 0;
+  let result = "i am result";
+
+  result = getResult();
+
   // JSX
   return (
     <div>
       <h1>App</h1>
-      <span>CodeMobiles</span>
+      <span>CodeMobiles {count} </span>
+      <span>{result}</span>
     </div>
   );
+}
+
+function getResult() {
+  return 1;
 }
