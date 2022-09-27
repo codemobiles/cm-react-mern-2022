42cb57e0a3c74a265d1debf58ecd7a82e90b7c04
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-09-27 10:32:19 +0700

    1234

--- demo1/src/components/pages/LoginPage/LoginPage.tsx
+++ demo1/src/components/pages/LoginPage/LoginPage.tsx
@@ -10,6 +10,17 @@ import { Stack } from "@mui/system";
 import * as React from "react";
 import { User } from "../../../types/user.type";
 import { Controller, useForm } from "react-hook-form";
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const formValidateSchema = Yup.object().shape({
+  // username: Yup.string().email("Invalid email address").required("Email is required").trim(),
+  username: Yup.string()
+    .min(4)
+    .required("Username must be more than 3 letters")
+    .trim(),
+  password: Yup.string().required("Password is required").trim(),
+});
 
 type LoginPageProps = {
   //
@@ -29,7 +40,7 @@ const LoginPage: React.FC<any> = () => {
 
   const showForm = () => {
     return (
-      <form onSubmit={handleSubmit(submit)}>
+      <form noValidate onSubmit={handleSubmit(submit)}>
         <Stack direction="column" spacing={1}>
           {/* Username */}
           <Controller
