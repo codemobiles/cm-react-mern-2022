8cea340b0400d6ff7f60923368506aa213abebc3
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-09-27 14:27:54 +0700

    1234

--- demo1/backend/server.js
+++ demo1/backend/server.js
@@ -1,1 +1,8 @@
-console.log("asdf");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ result: "ok" });
+});
+
+app.listen(8081, () => console.log("server is running."));
