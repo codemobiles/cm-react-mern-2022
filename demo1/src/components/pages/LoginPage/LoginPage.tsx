88d74f005eef9b0f0df2ffef1d0784c5abd823c5
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-09-27 09:11:06 +0700

    1234

--- demo1/src/components/pages/LoginPage/LoginPage.tsx
+++ demo1/src/components/pages/LoginPage/LoginPage.tsx
@@ -1,4 +1,12 @@
-import { Box, Button, Card, CardContent, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { Stack } from "@mui/system";
 import * as React from "react";
 
 type LoginPageProps = {
@@ -6,7 +14,6 @@ type LoginPageProps = {
 };
 
 const LoginPage: React.FC<any> = () => {
-  
   return (
     <Box>
       <Card>
@@ -14,6 +21,14 @@ const LoginPage: React.FC<any> = () => {
           <Typography gutterBottom variant="h5">
             Login
           </Typography>
+
+          <form>
+            <Stack direction="column" spacing={1}>
+              <TextField label="Username" variant="outlined" />
+              <TextField type="password" label="Password" variant="outlined" />
+              <Button variant="contained">Login</Button>
+            </Stack>
+          </form>
         </CardContent>
       </Card>
     </Box>
