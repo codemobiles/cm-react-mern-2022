f06ddbc8c9171bfdfdd66e07b83754f62ad39552
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-09-28 13:32:27 +0700

    1234

---
+++ demo1/backend-typeorm/src/controller/ProductController.ts
@@ -0,0 +1,11 @@
+import { NextFunction, Request, Response } from "express";
+import { Products } from "../entity/Products";
+import { AppDataSource } from "../data-source";
+
+export class ProductController {
+  private productRepository = AppDataSource.getMongoRepository(Products);
+
+  async all(req: Request, res: Response, next: NextFunction) {
+    return this.productRepository.find();
+  }
+}
