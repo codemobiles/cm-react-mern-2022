4fd33329e0a7e76e74e6cd49cf28fffbad1f031f
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-09-29 10:06:54 +0700

    1234

--- demo1/src/hooks/useCopyToClipboard.ts
+++ demo1/src/hooks/useCopyToClipboard.ts
@@ -1,1 +1,10 @@
-export default {};
+type useCopyToClipboardResult = [
+  isCopied: boolean,
+  handleCopy: (text: number | string) => void
+];
+
+export default function useCopyToClipboard(
+  resetInterval: number
+): useCopyToClipboardResult {
+  return [false, () => {}];
+}
