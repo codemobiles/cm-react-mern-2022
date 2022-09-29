28574a747489716eabc90c0759b3dcb161ec7d38
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-09-29 13:15:48 +0700

    1234

--- demo1/backend-typeorm/src/controller/ProductController.ts
+++ demo1/backend-typeorm/src/controller/ProductController.ts
@@ -1,11 +1,19 @@
 import { NextFunction, Request, Response } from "express";
 import { Products } from "../entity/Products";
 import { AppDataSource } from "../data-source";
+import * as formidable from "formidable";
 
 export class ProductController {
   private productRepository = AppDataSource.getMongoRepository(Products);
 
   async all(req: Request, res: Response, next: NextFunction) {
     return this.productRepository.find();
+  }
+
+  async add(req: Request, res: Response, next: NextFunction) {
+    const form = new formidable.IncomingForm();
+    form.parse(req, async (error, fields: any, files) => {
+      
+    });
   }
 }
