59c0cbee00d8c566265a8024d637ad31682dfd4e
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-09-29 09:33:04 +0700

    1234

--- demo1/src/components/pages/StockPage/StockPage.tsx
+++ demo1/src/components/pages/StockPage/StockPage.tsx
@@ -1,11 +1,21 @@
 import { Box } from "@mui/material";
 import * as React from "react";
+import { getProducts } from "../../../store/slices/stockSlice";
+import { useAppDispatch } from "../../../store/store";
 
 type StockPageProps = {
   //
 };
 
 const StockPage: React.FC<any> = () => {
+  const dispatch = useAppDispatch();
+
+  React.useEffect(() => {
+    // listen created event
+    console.log("Stock created");
+    dispatch(getProducts());
+  });
+
   const courses: string[] = ["angular", "react", "vue", "flutter"];
 
   return (
