804cd9aa756a986ea0b2bf28c149c10e2b9bd998
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-09-29 09:20:16 +0700

    1234

--- demo1/src/store/slices/stockSlice.ts
+++ demo1/src/store/slices/stockSlice.ts
@@ -1,4 +1,32 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { server } from "../../constants";
+import { httpClient } from "../../utils/HttpClient";
+
+export interface StockState {
+  stockAllResult: any[];
+  stockOneResult: any | null;
+}
+
+const initialState: StockState = {
+  stockAllResult: [],
+  stockOneResult: null,
+};
+
+// Query
+export const getProducts = createAsyncThunk(
+  "stock/getAll",
+  async (keyword?: string) => {
+    if (keyword) {
+      const result = await httpClient.get(
+        `${server.PRODUCT_URL}/name/${keyword}`
+      );
+      return result.data;
+    } else {
+      const result = await httpClient.get(server.PRODUCT_URL);
+      return result.data;
+    }
+  }
+);
 
 const stockSlice = createSlice({
   name: "stock",
